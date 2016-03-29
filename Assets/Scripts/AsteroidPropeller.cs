e0361888da8a1cd83bb4429bca5eda2cbfd8a7bd
vonszymon <vonszymon@gmail.com>
2016-03-30 02:32:02 +0800

    Added generation of asteroids

---
+++ Assets/Scripts/AsteroidPropeller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidPropeller : MonoBehaviour {
+
+	public float thrust;
+	public float maxRotationSpeed;
+	private Rigidbody rigidbody;
+	private Vector3 direction;
+	private float rotationX;
+	private float rotationY;
+	private float rotationZ;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		Vector3 vector = new Vector3 (Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+		direction = vector.normalized;
+		rotationX = maxRotationSpeed * Random.value;
+		rotationY = maxRotationSpeed * Random.value;
+		rotationZ = maxRotationSpeed * Random.value;
+	}
+
+	void FixedUpdate () {
+		rigidbody.AddForce (direction * thrust);
+	}
+
+	void Update() {
+		transform.Rotate (Time.deltaTime * rotationX, Time.deltaTime * rotationY, Time.deltaTime * rotationZ);
+	}
+}
---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        rb.AddForce(transform.forward * thrust);
+    }
+}
