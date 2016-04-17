9a1bb94762a8384fe9460bc3c03baf697225f5e6
vonszymon <vonszymon@gmail.com>
2016-04-17 17:02:20 +0200

    Added collision sound

---
+++ Assets/Scripts/CollisionSound.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+private var source : AudioSource; 
+
+function Start () {
+	var sources = GetComponents(AudioSource); 
+	source = sources[1];
+}
+
+function OnCollisionEnter (col : Collision)
+{
+	source.volume = 1.0;
+	source.Play();
+}
