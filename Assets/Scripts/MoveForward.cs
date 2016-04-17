9f07fa0faea74c0d8e58377f6941aeb0bf72239b
vonszymon <vonszymon@gmail.com>
2016-04-17 16:18:27 +0200

    Added ambient sound for some objects. Another spaceship model.

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -4,13 +4,8 @@ using System.Collections;
 public class MoveForward : MonoBehaviour
 {
     public float thrust;
-    public Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
     void Update()
     {
-        rb.AddForce(transform.forward * thrust);
+		transform.position += transform.forward * Time.deltaTime * thrust;
     }
 }
---
+++ Assets/Scripts/ObjectSound.js
@@ -0,0 +1,29 @@
+﻿private var player : GameObject;
+public var maxDistance : float;
+private var canPlayAudio = true;
+private var source : AudioSource;
+
+function Start () {
+	source = GetComponent.<AudioSource>();
+	player = GameObject.FindWithTag("Player");
+}
+
+function Update () {
+	var playerDistance = Vector3.Distance(player.transform.position, transform.position);
+ 	if (playerDistance < maxDistance) {
+ 		if (canPlayAudio) {
+ 			source.loop = true;
+ 			source.volume = (maxDistance - playerDistance) / maxDistance;
+ 			source.Play();
+ 			canPlayAudio = false;
+ 		} else {
+ 			source.volume = (maxDistance - playerDistance) / maxDistance;
+ 		}
+ 	} else {
+ 		if(!canPlayAudio){
+ 			source.Stop();
+ 			canPlayAudio = true;
+ 		}
+ 	}
+}
+
