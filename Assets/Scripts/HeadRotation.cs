5b444524ee7dd22af73e5fbc7ef7ed33ff717ecb
maguda <robert.maguda@gmail.com>
2016-04-26 21:05:09 +0200

    Fixed position of camera

---
+++ Assets/Scripts/HeadRotation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class HeadRotation : MonoBehaviour {
+	public bool updateEarly = false;
+
+	public delegate void HeadUpdatedDelegate(GameObject head);
+
+	public event HeadUpdatedDelegate OnHeadUpdated;
+
+	void Awake() {
+		Cardboard.Create();
+	}
+
+	private bool updated;
+
+	void Update() {
+		updated = false;
+		if (updateEarly) {
+			UpdateHead();
+		}
+	}
+
+	void LateUpdate() {
+		UpdateHead();
+	}
+
+	private void UpdateHead() {
+		if (updated) {
+			return;
+		}
+		updated = true;
+		Cardboard.SDK.UpdateState();
+
+		transform.localRotation = Cardboard.SDK.HeadPose.Orientation;
+
+		if (OnHeadUpdated != null) {
+			OnHeadUpdated(gameObject);
+		}
+	}
+}
