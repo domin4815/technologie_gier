088050a51d9aa52968ea0a0689ea460928d83e02
maguda <robert.maguda@gmail.com>
2016-06-07 09:36:24 +0200

    i tottaly fixed sound and asteroids all by myself

--- Assets/Scripts/ObjectSound.js
+++ Assets/Scripts/ObjectSound.js
@@ -10,14 +10,16 @@ function Start () {
 
 function Update () {
 	var playerDistance = Vector3.Distance(player.transform.position, transform.position);
- 	if (playerDistance < maxDistance) {
+ 	if (playerDistance < maxDistance + 200.0) {
  		if (canPlayAudio) {
  			source.loop = true;
- 			source.volume = (maxDistance - playerDistance) / maxDistance;
+ 			var distance = (maxDistance - playerDistance + 200.0) / maxDistance;
+ 			if(distance>0)
+ 				source.volume = distance;
  			source.Play();
  			canPlayAudio = false;
  		} else {
- 			source.volume = (maxDistance - playerDistance) / maxDistance;
+ 			source.volume = (maxDistance - playerDistance + 200.0) / maxDistance;
  		}
  	} else {
  		if(!canPlayAudio){
